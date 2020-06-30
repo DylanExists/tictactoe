1fe35cf81b153da0e256672adbb3049bc7cc4391
DylanExists <58371510+DylanExists@users.noreply.github.com>
2020-06-30 12:14:28 -0400

    optimized replacement code
    replaced 9 lines with 1

--- main.py
+++ main.py
@@ -35,15 +35,7 @@ while z == 0:
     h = "O"
     board()
     g = input(">>>")
-    if g == "1" or g == "2" or g == "3" and a[g] != "X" or "O":
-        replacea(g, g, h)
-    elif g == "4" or g == "5" or g == "6" and a[g] != "X" or "O":
-        replaceb(g, g, h)
-    elif g == "7" or g == "8" or g == "9" and a[g] != "X" or "O":
-        replacec(g, g, h)
-    else:
-        print("you did a bad")
-        bad = 42 / 0
+    replacea(g, g, h)
     if a[0, 1, 2] or a[3, 4, 5] or a[6, 7, 8] == "X" or "O":
         z = 1
     elif a[0, 3, 6] or a[1, 4, 7] or a[2, 5, 8] == "X" or "O":
