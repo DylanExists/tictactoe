02b6457e6395b873e2e32678745d0752a2134049
DylanExists <58371510+DylanExists@users.noreply.github.com>
2020-06-30 13:29:01 -0400

    fixed displacement
    fixed problem where placement was displaced by 1 and minor changes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ z = 0
 a = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-def replacea(d, e, f):
+def replace(d, e, f):
     a.remove(d)
-    a.insert(int(e), f)
+    a.insert(int(e) - 1, f)
 
 
 def board():
@@ -17,7 +17,7 @@ while z == 0:
     h = "X"
     board()
     g = input(">>>")
-    replacea(g, g, h)
+    replace(g, g, h)
     i = 0
     while i < 5:
         if a[i] and a[i + 1] and a[i + 2] == "X" or "O":
@@ -35,7 +35,7 @@ while z == 0:
     h = "O"
     board()
     g = input(">>>")
-    replacea(g, g, h)
+    replace(g, g, h)
     i = 0
     while i < 5:
         if a[i] and a[i + 1] and a[i + 2] == "X" or "O":
@@ -50,6 +50,6 @@ while z == 0:
         i += 1
     if a[0] and a[4] and a[7] or a[2] and a[4] and a[6] == "X" or "O":
         z = 1
-print("Someone won! You know who.")
+print("WIN!")
 print("Ending board:")
 board()
