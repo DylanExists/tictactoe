e48a5675d9d9ad0acdc301895327e2f501b95f7f
DylanExists <58371510+DylanExists@users.noreply.github.com>
2020-06-30 21:19:46 -0400

    fixed win check
    removed unnecessary and operators that created errors
    first version to work correctly

--- main.py
+++ main.py
@@ -13,27 +13,47 @@ def board():
     print(a[6], a[7], a[8])
 
 
-while z == 0:
+while 1 < 2:
     h = "X"
     board()
     g = input(">>>")
     replace(g, g, h)
-    if a[0] and a[3] and a[6] or a[1] and a[4] and a[7] or a[2] and a[5] and a[8] == "X" or "O":
-        z = 1
-    elif a[0] and a[1] and a[2] or a[3] and a[4] and a[5] or a[6] and a[7] and a[8] == "X" or "O":
-        z = 1
-    elif a[0] and a[4] and a[7] or a[2] and a[4] and a[6] == "X" or "O":
-        z = 1
+    if a[0] == h and a[1] == h and a[2] == h:
+        break
+    elif a[3] == h and a[4] == h and a[5] == h:
+        break
+    elif a[6] == h and a[7] == h and a[8] == h:
+        break
+    elif a[0] == h and a[3] == h and a[6] == h:
+        break
+    elif a[1] == h and a[4] == h and a[7] == h:
+        break
+    elif a[2] == h and a[5] == h and a[8] == h:
+        break
+    elif a[0] == h and a[4] == h and a[8] == h:
+        break
+    elif a[2] == h and a[4] == h and a[6] == h:
+        break
     h = "O"
     board()
     g = input(">>>")
     replace(g, g, h)
-    if a[0] and a[3] and a[6] or a[1] and a[4] and a[7] or a[2] and a[5] and a[8] == "X" or "O":
-        z = 1
-    elif a[0] and a[1] and a[2] or a[3] and a[4] and a[5] or a[6] and a[7] and a[8] == "X" or "O":
-        z = 1
-    elif a[0] and a[4] and a[7] or a[2] and a[4] and a[6] == "X" or "O":
-        z = 1
+    if a[0] == h and a[1] == h and a[2] == h:
+        break
+    elif a[3] == h and a[4] == h and a[5] == h:
+        break
+    elif a[6] == h and a[7] == h and a[8] == h:
+        break
+    elif a[0] == h and a[3] == h and a[6] == h:
+        break
+    elif a[1] == h and a[4] == h and a[7] == h:
+        break
+    elif a[2] == h and a[5] == h and a[8] == h:
+        break
+    elif a[0] == h and a[4] == h and a[8] == h:
+        break
+    elif a[2] == h and a[4] == h and a[6] == h:
+        break
 print("WIN!")
 print("Ending board:")
 board()
